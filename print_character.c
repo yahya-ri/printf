ff75741cfc77b744856037d2c6aa6a8ee5408dd6
Yahya <yahyaoulha8@gmail.com>
2023-08-21 03:15:26 -0700

    attemted to fix it for check 15

--- print_character.c
+++ print_character.c
@@ -7,6 +7,17 @@
  */
 int prt_c(va_list args)
 {
-	_putchar(va_arg(args, int));
-	return (1);
+	char str;
+
+	str = va_arg(args, int);
+
+	if (str == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		_putchar(str);
+		return (1);
+	}
 }
